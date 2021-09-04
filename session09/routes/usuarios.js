37a7694c77e83cc7f125ebce5906e9b33e1b0af9
Gustavo Ariel <ariel256@gmail.com>
2021-09-04 09:02:16 -0300

    Validate with Joi

--- session09/routes/usuarios.js
+++ session09/routes/usuarios.js
@@ -1,41 +1,96 @@
 const express = require('express');
 const Usuario = require('../models/usuario_model');
-const ruta = express.Router();
+const Joi     = require('joi');
+const ruta    = express.Router();
+
+const schema = Joi.object({
+  nombre: Joi.string()
+      .min(3)
+      .max(10)
+      .required(),
+
+  password: Joi.string()
+      .pattern(/^[a-zA-Z0-9]{3,30}$/),
+
+  email: Joi.string()
+      .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
+});
 
 ruta.get('/', (req, res) => {
-  res.json('Listo el GET de usuarios')
+  let resultado = listarUsuariosActivos();
+  resultado.then(usuarios => {
+    res.json(usuarios)
+  }).catch(err => {
+    res.status(400).json({
+      error: err
+    })
+  })
 })
 
 ruta.post('/', (req, res) => {
   let body = req.body
-  let resultado = crearUsuario(body)
-  
-  resultado.then( user => {
-    res.json({
-      valor: user
+
+  const {error, value} = schema.validate({nombre: body.nombre, email: body.email})
+
+  if(!error){
+    let resultado = crearUsuario(body)
+    
+    resultado.then( user => {
+      res.json({
+        valor: user
+      })
+    }).catch( err => {
+      res.status(400).json({
+        error: err
+      })
     })
-  }).catch( err => {
+  }
+  else{
     res.status(400).json({
-      error: err
+      error: error
     })
-  })
+  }
 })
 
 ruta.put('/:email', (req, res) => {
-  let resultado = actualizarUsuario(req.params.email, req.body)
-
-  console.log("body: " + req.body.nombre + " - " + req.body.password)
-
-  resultado.then(valorP => {
-    res.json({
-      valor: valorP
+  
+  const {error, value} = schema.validate({nombre: req.body.nombre})
+  
+  if(!error){
+    let resultado = actualizarUsuario(req.params.email, req.body)
+    resultado.then(valorP => {
+      res.json({
+        valor: valorP
+      })
+    }).catch( err => {
+      res.status(400).json({
+        error: err
+      })
     })
-  }).catch( err => {
+  }
+  else {
+    res.status(400).json({
+      error: error
+    })
+  }
+})
+
+ruta.delete('/:email', (req, res) => {
+  let resultado = desactivarUsuario(req.params.email)
+  resultado.then( valor => {
+    res.json({ usuario: valor })
+  }).catch(err => {
     res.status(400).json({
       error: err
     })
   })
 })
+
+
+async function listarUsuariosActivos(){
+  let usuarios = await Usuario.find({"estado": true})
+  return usuarios;
+}
 
 async function crearUsuario(body){
   let usuario = new Usuario({
@@ -53,19 +108,18 @@ async function actualizarUsuario(email, body){
     password: body.password
   }
 
-  console.log(update)
-  
-  // let usuario = await Usuario.findOneAndUpdate({"email": email}, {
-  //   $set: {
-  //     nombre  : body.nombre,
-  //     password: body.password
-  //   }
-  // }, {new: true})
-
   let usuarioP = await Usuario.findOneAndUpdate(filter, update, {
     new: true
   })
   return usuarioP;
+}
+
+async function desactivarUsuario(email){
+  const filter = { 'email' : email }
+  const update = { 'estado' : false}
+  let usuario = await Usuario.findOneAndUpdate(filter, update, { new: true})
+
+  return usuario;
 }
 
 module.exports = ruta;
